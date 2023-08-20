a982869efe1ba1520d4aa5c9d3f9c834437fa88f
Pwongchaiya <Pwongchaiya1@gmail.com>
2023-08-19 21:06:54 -0700

    CODERUB: Added Banner

--- InternTrack.Portal.Web/Program.cs
+++ InternTrack.Portal.Web/Program.cs
@@ -1,3 +1,8 @@
+// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
