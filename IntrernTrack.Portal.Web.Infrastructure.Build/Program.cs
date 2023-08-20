31748832ac8a9bbc238d66b54d4a9bfac3178eca
Pwongchaiya <Pwongchaiya1@gmail.com>
2023-08-19 20:21:11 -0700

    INFRA: Setup Build

--- IntrernTrack.Portal.Web.Infrastructure.Build/Program.cs
+++ IntrernTrack.Portal.Web.Infrastructure.Build/Program.cs
@@ -1,2 +1,83 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+using System.Collections.Generic;
+using ADotNet.Clients;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks.SetupDotNetTaskV1s;
+
+namespace InternTrack.Portal.Web.Infrastructure.Build
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var client = new ADotNetClient();
+
+            var gitHubPipeline = new GithubPipeline
+            {
+                Name = "Intern Portal Build",
+
+                OnEvents = new Events
+                {
+                    Push = new PushEvent
+                    {
+                        Branches = new string[] { "main" }
+                    },
+
+                    PullRequest = new PullRequestEvent
+                    {
+                        Branches = new string[] { "main" }
+                    }
+                },
+
+                Jobs = new Jobs
+                {
+                    Build = new BuildJob
+                    {
+                        RunsOn = BuildMachines.WindowsLatest,
+
+                        Steps = new List<GithubTask>
+                        {
+                            new CheckoutTaskV2
+                            {
+                                Name = "Checking Out Code"
+                            },
+
+                            new SetupDotNetTaskV1
+                            {
+                                Name = "Installing .NET",
+
+                                TargetDotNetVersion = new TargetDotNetVersion
+                                {
+                                    DotNetVersion = "7.0.306"
+                                }
+                            },
+
+                            new RestoreTask
+                            {
+                                Name = "Restoring Nuget Packages"
+                            },
+
+                            new DotNetBuildTask
+                            {
+                                Name = "Building Project"
+                            },
+
+                            new TestTask
+                            {
+                                Name = "Running Tests"
+                            }
+                        },
+                    }
+                }
+            };
+
+            client.SerializeAndWriteToFile(adoPipeline: gitHubPipeline,
+                path: "../../../../.github/workflows/dotnet.yml");
+        }
+    }
+}
