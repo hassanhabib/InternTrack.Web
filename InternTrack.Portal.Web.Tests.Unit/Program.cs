934310866c3d7e44d18c890621c28214b510fe70
Pwongchaiya <Pwongchaiya1@gmail.com>
2023-08-19 19:43:21 -0700

    INFRA: Installed NuGet Packages for Testing

--- InternTrack.Portal.Web.Tests.Unit/Program.cs
+++ InternTrack.Portal.Web.Tests.Unit/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+namespace InternTrack.Portal.Web.Tests.Unit
+{
+    public class Program
+    {
+
+    }
+}
