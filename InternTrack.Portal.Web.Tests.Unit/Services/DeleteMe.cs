4f8922cf27d52304f5b5ca2a6cbd4577be495780
Pwongchaiya <Pwongchaiya1@gmail.com>
2023-08-19 19:51:41 -0700

    INFRA: Created Services Folder and Added DeleteMe file

---
+++ InternTrack.Portal.Web.Tests.Unit/Services/DeleteMe.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace InternTrack.Portal.Web.Tests.Unit.Services
+{
+    public class DeleteMe
+    {
+        [Fact]
+        private void ShouldBeTrue() => Assert.True(condition: true);
+    }
+}
