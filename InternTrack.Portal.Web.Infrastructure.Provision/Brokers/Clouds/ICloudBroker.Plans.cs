995c657ddfa874a963f55d7e12f6689b812f17a5
Pwongchaiya <Pwongchaiya1@gmail.com>
2023-08-21 20:44:37 -0700

    BROKERS: Plans Initialized

---
+++ InternTrack.Portal.Web.Infrastructure.Provision/Brokers/Clouds/ICloudBroker.Plans.cs
@@ -0,0 +1,18 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+using System.Threading.Tasks;
+using Azure.ResourceManager.AppService;
+using Azure.ResourceManager.Resources;
+
+namespace InternTrack.Portal.Web.Infrastructure.Provision.Brokers.Clouds
+{
+    public partial interface ICloudBroker
+    {
+        ValueTask<AppServicePlanResource> CreatePlanAsync(
+            string planName,
+            ResourceGroupResource resourceGroup);
+    }
+}
