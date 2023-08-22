a430ddf070951cad87723ce43336e700ebc998d5
Pwongchaiya <Pwongchaiya1@gmail.com>
2023-08-21 21:12:54 -0700

    FOUNDATIONS: CloudManagementService Initialized

---
+++ InternTrack.Portal.Web.Infrastructure.Provision/Services/ICloudManagementService.cs
@@ -0,0 +1,52 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+using Azure.ResourceManager.ApplicationInsights;
+using Azure.ResourceManager.AppService;
+using Azure.ResourceManager.Resources;
+using Azure.ResourceManager.Sql;
+using InternTrack.Portal.Web.Infrastructure.Provision.Models.Storages;
+using System.Threading.Tasks;
+
+namespace InternTrack.Portal.Web.Infrastructure.Provision.Services
+{
+    public interface ICloudManagementService
+    {
+        ValueTask<ResourceGroupResource> ProvisionResourceGroupAsync(
+                string projectName,
+                string environment);
+
+        ValueTask<AppServicePlanResource> ProvisionPlanAsync(
+            string projectName,
+            string environment,
+            ResourceGroupResource resourceGroup);
+
+        ValueTask<SqlServerResource> ProvisionSqlServerAsync(
+            string projectName,
+            string environment,
+            ResourceGroupResource resourceGroup);
+
+        ValueTask<SqlDatabase> ProvisionSqlDatabaseAsync(
+            string projectName,
+            string environment,
+            SqlServerResource sqlServer);
+
+        ValueTask<WebSiteResource> ProvisionWebAppAsync(
+            string projectName,
+            string environment,
+            string databaseConnectionString,
+            ResourceGroupResource resourceGroup,
+            AppServicePlanResource appServicePlan);
+
+        ValueTask<ApplicationInsightsComponentResource> ProvisionApplicationInsightComponentAsync(
+            string projectName,
+            string environment,
+            ResourceGroupResource resourceGroup);
+
+        ValueTask DeprovisionResourceGroupAsync(
+            string projectName,
+            string environment);
+    }
+}
