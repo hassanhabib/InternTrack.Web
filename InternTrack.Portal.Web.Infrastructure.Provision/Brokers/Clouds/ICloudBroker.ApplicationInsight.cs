2dc0f4c7c1fac9d52fbd312f81a1c815884ce402
Pwongchaiya <Pwongchaiya1@gmail.com>
2023-08-21 20:54:52 -0700

    BROKERS: ApplicationInsight Initialized

---
+++ InternTrack.Portal.Web.Infrastructure.Provision/Brokers/Clouds/ICloudBroker.ApplicationInsight.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternTrack.Portal.Web.Infrastructure.Provision.Brokers.Clouds
+{
+    public interface ICloudBroker
+    {
+    }
+}
