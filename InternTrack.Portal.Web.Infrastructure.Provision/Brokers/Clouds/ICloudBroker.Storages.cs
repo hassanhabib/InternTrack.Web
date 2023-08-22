49e16215a449c05933c9847592c4a7150c70e20e
Pwongchaiya <Pwongchaiya1@gmail.com>
2023-08-21 20:48:32 -0700

    BROKERS: Storages Initialized

---
+++ InternTrack.Portal.Web.Infrastructure.Provision/Brokers/Clouds/ICloudBroker.Storages.cs
@@ -0,0 +1,25 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+using Azure.ResourceManager.Resources;
+using Azure.ResourceManager.Sql;
+using InternTrack.Portal.Web.Infrastructure.Provision.Models.Storages;
+using System.Threading.Tasks;
+
+namespace InternTrack.Portal.Web.Infrastructure.Provision.Brokers.Clouds
+{
+    public partial interface ICloudBroker
+    {
+        ValueTask<SqlServerResource> CreateSqlServerAsync(
+            string sqlServerName,
+            ResourceGroupResource resourceGroup);
+
+        ValueTask<SqlDatabaseResource> CreateSqlDataBaseAsync(
+            string sqlDatabaseName,
+            SqlServerResource sqlServer);
+
+        SqlDatabaseAccess GetAdminAccess();
+    }
+}
