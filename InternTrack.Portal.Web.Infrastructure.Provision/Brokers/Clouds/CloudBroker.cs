f0fc5b3188d80626a9f1ee7e7046bcce56313d42
Pwongchaiya <Pwongchaiya1@gmail.com>
2023-08-21 20:29:27 -0700

    BROKERS: CloudBroker

---
+++ InternTrack.Portal.Web.Infrastructure.Provision/Brokers/Clouds/CloudBroker.cs
@@ -0,0 +1,26 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+using System;
+using Azure.Identity;
+using Azure.ResourceManager;
+
+namespace InternTrack.Portal.Web.Infrastructure.Provision.Brokers.Clouds
+{
+    public partial class CloudBroker : ICloudBroker
+    {
+        private readonly string adminName;
+        private readonly string adminPassword;
+        private readonly ArmClient client;
+        private readonly EnvironmentCredential environmentCredential;
+
+        public CloudBroker()
+        {
+            this.adminName = Environment.GetEnvironmentVariable("Azure_Admin_Name");
+            this.adminPassword = Environment.GetEnvironmentVariable("Azure_Admin_Password");
+            this.client = new ArmClient(environmentCredential);
+        }
+    }
+}
