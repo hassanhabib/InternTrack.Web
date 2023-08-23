2706bf040a55bc30fc623ae8625a2dd5d78d2cc4
adi latic <alatic1234@gmail.com>
2023-08-23 17:47:01 -0500

    Brokers: Added Datetimes

---
+++ InternTrack.Portal.Web/Brokers/DateTimes/DateTimeBroker.cs
@@ -0,0 +1,19 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+using System;
+
+namespace InternTrack.Portal.Web.Brokers.DateTimes
+{
+    public class DateTimeBroker : IDateTimeBroker
+    {
+        public DateTimeOffset GetCurrentDateTimeOffset() => DateTimeOffset.UtcNow;
+
+        public DateTimeOffset GetCurrentDateTimeOffSet()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
