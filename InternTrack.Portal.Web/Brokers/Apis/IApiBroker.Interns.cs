98f0c4cd367da4e6d1bc8e17f8c705249ced0aba
Pwongchaiya <Pwongchaiya1@gmail.com>
2023-08-24 14:43:45 -0700

    BROKERS: IApiBroker.Interns Implemented

---
+++ InternTrack.Portal.Web/Brokers/Apis/IApiBroker.Interns.cs
@@ -0,0 +1,16 @@
+﻿using InternTrack.Portal.Web.Models.Interns;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace InternTrack.Portal.Web.Brokers.Apis
+{
+    public partial interface IApiBroker
+    {
+        ValueTask<Intern> PostInternAsync(Intern intern);
+        ValueTask<List<Intern>> GetAllInternsAsync();
+        ValueTask<Intern> GetInternByIdAsync(Guid internId);
+        ValueTask<Intern> PutInternAsync(Intern intern);
+        ValueTask<Intern> DeleteInternByIdAsync(Guid internId);
+    }
+}
