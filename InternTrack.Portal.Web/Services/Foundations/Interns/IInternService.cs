ee106ece7e4cbfffbc688c4a706ada4a877a04a9
Pwongchaiya <Pwongchaiya1@gmail.com>
2023-08-25 20:26:55 -0700

    FOUNDATIONS: Intern Broker Dependency Injection Initialized

--- InternTrack.Portal.Web/Services/Foundations/Interns/IInternService.cs
+++ InternTrack.Portal.Web/Services/Foundations/Interns/IInternService.cs
@@ -1,4 +1,9 @@
-﻿namespace InternTrack.Portal.Web.Services.Foundations.Interns
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+namespace InternTrack.Portal.Web.Services.Foundations.Interns
 {
     public interface IInternService
     {
--- InternTrack.Portal.Web/Services/Foundations/Interns/InternService.cs
+++ InternTrack.Portal.Web/Services/Foundations/Interns/InternService.cs
@@ -1,6 +1,22 @@
-﻿namespace InternTrack.Portal.Web.Services.Foundations.Interns
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using InternTrack.Portal.Web.Brokers.Apis;
+using InternTrack.Portal.Web.Brokers.Loggings;
+
+namespace InternTrack.Portal.Web.Services.Foundations.Interns
 {
-    public class InternService
+    public class InternService : IInternService
     {
+        private readonly IApiBroker apiBroker;
+        private readonly ILoggingBroker loggingBroker;
+
+        public InternService(IApiBroker apiBroker, ILoggingBroker loggingBroker)
+        {
+            this.apiBroker = apiBroker;
+            this.loggingBroker = loggingBroker;
+        }
     }
 }
