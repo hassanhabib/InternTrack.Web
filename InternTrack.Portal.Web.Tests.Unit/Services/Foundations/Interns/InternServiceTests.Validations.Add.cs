d9be06b6bbc5105e065e0dc256c0539997041014
Pwongchaiya <Pwongchaiya1@gmail.com>
2023-08-27 22:39:39 -0700

    ShouldThrowValidationExceptionOnAddWhenInternIsNullAndLogItAsync -> FAIL

---
+++ InternTrack.Portal.Web.Tests.Unit/Services/Foundations/Interns/InternServiceTests.Validations.Add.cs
@@ -0,0 +1,59 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using InternTrack.Portal.Web.Models.Interns;
+using InternTrack.Portal.Web.Models.Interns.Exceptions;
+using Moq;
+using Xunit;
+
+namespace InternTrack.Portal.Web.Tests.Unit.Services.Foundations.Interns
+{
+    public partial class InternServiceTests
+    {
+        [Fact]
+        public async void ShouldThrowValidationExceptionOnAddWhenInternIsNullAndLogItAsync()
+        {
+            //given
+            Intern nullIntern = null;
+            var someException = new Exception();
+
+            var nullInternException = new NullInternException(
+                message: "The Intern is null.",
+                    innerException: someException);
+
+            var expectedInternValidationException =
+                new InternValidationException(
+                    message: "Intern validation error occurred. Please, try again.",
+                        innerException: nullInternException);
+
+            //when
+            ValueTask<Intern> createInternTask =
+                this.internService.AddInternAsync(nullIntern);
+
+            var actualInternValidationException =
+                await Assert.ThrowsAsync<InternValidationException>(() =>
+                    createInternTask.AsTask());
+
+            //then
+            actualInternValidationException.Should()
+                .BeEquivalentTo(expectedInternValidationException);
+
+            this.apiBrokerMock.Verify(broker =>
+                broker.PostInternAsync(It.IsAny<Intern>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameAsExceptionAs(
+                    expectedInternValidationException))),
+                        Times.Once);
+
+            this.apiBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ InternTrack.Portal.Web/Models/Interns/Exceptions/InternValidationException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xeptions;
+
+namespace InternTrack.Portal.Web.Models.Interns.Exceptions
+{
+    public class InternValidationException : Xeption
+    {
+        public InternValidationException(Xeption innerException)
+            : base("Intern validation error occurred. Please, try again.",
+                  innerException)
+        { }
+
+        public InternValidationException(string message, Xeption innerException)
+            : base(message, innerException)
+        { }
+    }
+}
