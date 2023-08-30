89d4f3c838c8e1ce1ccdef479eb50de825d815e5
Pwongchaiya <Pwongchaiya1@gmail.com>
2023-08-29 23:15:34 -0700

    VIEWS: InternViewService Initalized

---
+++ InternTrack.Portal.Web/Services/Views/InternViews/InternViewService.cs
@@ -0,0 +1,28 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+using InternTrack.Portal.Web.Brokers.DateTimes;
+using InternTrack.Portal.Web.Brokers.Loggings;
+using InternTrack.Portal.Web.Services.Foundations.Interns;
+
+namespace InternTrack.Portal.Web.Services.Views.InternViews
+{
+    public partial class InternViewService : IInternViewService
+    {
+        private readonly IInternService internService;
+        private readonly IDateTimeBroker dateTimeBroker;
+        private readonly ILoggingBroker loggingBroker;
+
+        public InternViewService(
+            IInternService internService, 
+            IDateTimeBroker dateTimeBroker,
+            ILoggingBroker loggingBroker)
+        {
+            this.internService = internService;
+            this.dateTimeBroker = dateTimeBroker;
+            this.loggingBroker = loggingBroker;
+        }
+    }
+}
