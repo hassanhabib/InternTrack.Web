263862c77c44f869529d1c63bc2a6a274a28aba7
Pwongchaiya <Pwongchaiya1@gmail.com>
2023-08-31 11:27:48 -0700

    FOUNDATION: InternViewServiceTests Dependency Injected

--- InternTrack.Portal.Web.Tests.Unit/Services/Foundations/InternViews/InternViewServiceTests.cs
+++ InternTrack.Portal.Web.Tests.Unit/Services/Foundations/InternViews/InternViewServiceTests.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+using InternTrack.Portal.Web.Brokers.DateTimes;
+using InternTrack.Portal.Web.Brokers.Loggings;
+using InternTrack.Portal.Web.Services.Foundations.Interns;
+using InternTrack.Portal.Web.Services.Foundations.Users;
+using InternTrack.Portal.Web.Services.Views.InternViews;
+using Moq;
 
 namespace InternTrack.Portal.Web.Tests.Unit.Services.Foundations.InternViews
 {
-    internal class InternViewServiceTests
+    public partial class InternViewServiceTests
     {
+        private readonly Mock<IInternService> internServiceMock;
+        private readonly Mock<IUserService> userServiceMock;
+        private readonly Mock<IDateTimeBroker> dateTimeBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly IInternViewService internViewService;
+
+        public InternViewServiceTests()
+        {
+            this.internServiceMock = new Mock<IInternService>();
+            this.userServiceMock = new Mock<IUserService>();
+            this.dateTimeBrokerMock = new Mock<IDateTimeBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+
+            this.internViewService = new InternViewService(
+                internService: this.internServiceMock.Object,
+                dateTimeBroker: this.dateTimeBrokerMock.Object,
+                loggingBroker: this.loggingBrokerMock.Object,
+                userService: this.userServiceMock.Object);
+        }
     }
 }
--- InternTrack.Portal.Web/Services/Views/InternViews/InternViewService.cs
+++ InternTrack.Portal.Web/Services/Views/InternViews/InternViewService.cs
@@ -8,23 +8,27 @@ using InternTrack.Portal.Web.Brokers.DateTimes;
 using InternTrack.Portal.Web.Brokers.Loggings;
 using InternTrack.Portal.Web.Models.InternViews;
 using InternTrack.Portal.Web.Services.Foundations.Interns;
+using InternTrack.Portal.Web.Services.Foundations.Users;
 
 namespace InternTrack.Portal.Web.Services.Views.InternViews
 {
     public partial class InternViewService : IInternViewService
     {
         private readonly IInternService internService;
+        private readonly IUserService userService;
         private readonly IDateTimeBroker dateTimeBroker;
         private readonly ILoggingBroker loggingBroker;
 
         public InternViewService(
-            IInternService internService, 
+            IInternService internService,
             IDateTimeBroker dateTimeBroker,
-            ILoggingBroker loggingBroker)
+            ILoggingBroker loggingBroker,
+            IUserService userService)
         {
             this.internService = internService;
             this.dateTimeBroker = dateTimeBroker;
             this.loggingBroker = loggingBroker;
+            this.userService = userService;
         }
 
         public ValueTask<InternView> AddInternViewAsync(InternView internView)
