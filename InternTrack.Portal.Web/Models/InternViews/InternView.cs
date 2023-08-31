5589ecf02fdbdbe46480ff2ad87b491e40c4893d
Pwongchaiya <Pwongchaiya1@gmail.com>
2023-08-31 11:02:52 -0700

    DATA: InternView Initialized

---
+++ InternTrack.Portal.Web/Models/InternViews/InternView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace InternTrack.Portal.Web.Models.InternViews
+{
+    public class InternView
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Status { get; set; }
+        public DateTimeOffset JoinDate { get; set; }
+    }
+}
