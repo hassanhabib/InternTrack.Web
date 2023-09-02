73f1d8367da1591fd81bbf531fa2b388961de8ee
Pwongchaiya <Pwongchaiya1@gmail.com>
2023-09-01 20:57:17 -0700

    BROKER: NavigationBroker Logic Implemented

--- InternTrack.Portal.Web/Brokers/Navigations/INavigationBroker.cs
+++ InternTrack.Portal.Web/Brokers/Navigations/INavigationBroker.cs
@@ -1,6 +1,12 @@
-﻿namespace InternTrack.Portal.Web.Brokers.Navigations
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+namespace InternTrack.Portal.Web.Brokers.Navigations
 {
     public interface INavigationBroker
     {
+        void NavigateTo(string route);
     }
 }
--- InternTrack.Portal.Web/Brokers/Navigations/NavigationBroker.cs
+++ InternTrack.Portal.Web/Brokers/Navigations/NavigationBroker.cs
@@ -1,6 +1,23 @@
-﻿namespace InternTrack.Portal.Web.Brokers.Navigations
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using Microsoft.AspNetCore.Components;
+
+namespace InternTrack.Portal.Web.Brokers.Navigations
 {
-    public class NavigationBroker
+    public class NavigationBroker : INavigationBroker
     {
+
+        private readonly NavigationManager navigationManager;
+
+        public NavigationBroker(NavigationManager navigationManager)
+        {
+            this.navigationManager = navigationManager;
+        }
+        
+        public void NavigateTo(string route) =>
+            this.navigationManager.NavigateTo(route);
     }
 }
