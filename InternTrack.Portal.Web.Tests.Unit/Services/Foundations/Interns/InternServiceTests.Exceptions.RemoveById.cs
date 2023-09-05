15cbbc0fa00b71548bec87d1f4a1f6004b9360c7
Pwongchaiya <Pwongchaiya1@gmail.com>
2023-09-05 13:13:41 -0700

    ShouldThrowDependencyValidationExceptionOnRemoveIfInternIsNotFoundAndLogItAsync -> FAIL

--- InternTrack.Portal.Web.Tests.Unit/Services/Foundations/Interns/InternServiceTests.Exceptions.RemoveById.cs
+++ InternTrack.Portal.Web.Tests.Unit/Services/Foundations/Interns/InternServiceTests.Exceptions.RemoveById.cs
@@ -11,6 +11,8 @@ using InternTrack.Portal.Web.Models.Interns.Exceptions;
 using InternTrack.Portal.Web.Models.Interns;
 using Moq;
 using FluentAssertions;
+using RESTFulSense.Exceptions;
+using System.Net.Http;
 
 namespace InternTrack.Portal.Web.Tests.Unit.Services.Foundations.Interns
 {
@@ -18,7 +20,8 @@ namespace InternTrack.Portal.Web.Tests.Unit.Services.Foundations.Interns
     {
         [Theory]
         [MemberData(nameof(CriticalDependencyException))]
-        public async Task ShouldThrowCriticalDependencyExceptionOnRemoveIfCriticalErrorOccursAndLogItAsync(
+        private async Task 
+            ShouldThrowCriticalDependencyExceptionOnRemoveIfCriticalErrorOccursAndLogItAsync(
             Exception criticalDependencyException)
         {
             // given
@@ -57,6 +60,59 @@ namespace InternTrack.Portal.Web.Tests.Unit.Services.Foundations.Interns
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogCritical(It.Is(SameExceptionAs(
                     expectedInternDependencyException))),
+                        Times.Once);
+
+            this.apiBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        private async Task
+            ShouldThrowDependencyValidationExceptionOnRemoveIfInternIsNotFoundAndLogItAsync()
+        {
+            // given
+            Guid someInternId = Guid.NewGuid();
+            string responseMessage = GetRandomMessage();
+            var httpResponseMessage = new HttpResponseMessage();
+
+            var httpResponseNotFoundException =
+                new HttpResponseNotFoundException(
+                    httpResponseMessage,
+                        responseMessage);
+
+            var notFoundInternException =
+                new NotFoundInternException(
+                    message: "Not found Intern error occurred, please try again.",
+                        innerException: httpResponseNotFoundException);
+
+            var expectedInternDependencyValidationException =
+                new InternDependencyValidationException(
+                    message: "Intern dependency validation error occurred, please try again.",
+                        innerException: notFoundInternException);
+
+            this.apiBrokerMock.Setup(broker =>
+                broker.DeleteInternByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(httpResponseNotFoundException);
+
+            // when
+            ValueTask<Intern> removeInternByIdTask =
+                this.internService.RemoveInternByIdAsync(someInternId);
+
+            InternDependencyValidationException actualInternDependencyValidationException =
+                await Assert.ThrowsAsync<InternDependencyValidationException>(
+                    removeInternByIdTask.AsTask);
+
+            //then
+            actualInternDependencyValidationException.Should()
+                .BeEquivalentTo(expectedInternDependencyValidationException);
+
+            this.apiBrokerMock.Verify(broker =>
+                broker.DeleteInternByIdAsync(It.IsAny<Guid>()),
+                 Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedInternDependencyValidationException))),
                         Times.Once);
 
             this.apiBrokerMock.VerifyNoOtherCalls();
---
+++ InternTrack.Portal.Web/Models/Interns/Exceptions/NotFoundInternException.cs
@@ -0,0 +1,22 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System;
+using Xeptions;
+
+namespace InternTrack.Portal.Web.Models.Interns.Exceptions
+{
+    public class NotFoundInternException : Xeption
+    {
+        public NotFoundInternException(Exception innerException)
+        : base(message: "Not found Intern error occurred, please try again.",
+                  innerException)
+        { }
+
+        public NotFoundInternException(string message, Exception innerException) 
+            : base(message, innerException) 
+        { }
+    }
+}
