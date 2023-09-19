9941825e0b8ecfe98bfab9106e9c85db64affcc0
adi latic <alatic1234@gmail.com>
2023-09-19 14:41:43 -0500

    ShouldThrowValidationExceptionOnModifyWhenInternIsNullAndLogItAsync -> FAIL

---
+++ InternTrack.Portal.Web.Tests.Unit/Services/Foundations/Interns/InternServiceTests.Validations.Modify.cs
@@ -0,0 +1,50 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InternTrack.Portal.Web.Models.Interns;
+using InternTrack.Portal.Web.Models.Interns.Exceptions;
+using Moq;
+using Xunit;
+
+namespace InternTrack.Portal.Web.Tests.Unit.Services.Foundations.Interns
+{
+    public partial class InternServiceTests
+    {
+        [Fact]
+        public async void ShouldThrowValidationExceptionOnModifyWhenInternIsNullAndLogItAsync()
+        {
+            // given
+            Intern nullIntern = null;
+            var nullInternException = new NullInternException();
+
+            var expectedInternValidationException =
+                new InternValidationException(nullInternException);
+
+            // when
+            ValueTask<Intern> modifyInternTask =
+                this.internService.ModifyInternAsync(nullIntern);
+
+            // then
+            await Assert.ThrowsAsync<InternValidationException>(() =>
+                modifyInternTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedInternValidationException))),
+                    Times.Once);
+
+            this.apiBrokerMock.Verify(broker =>
+                broker.PutInternAsync(It.IsAny<Intern>()),
+                    Times.Never);
+
+            this.apiBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
