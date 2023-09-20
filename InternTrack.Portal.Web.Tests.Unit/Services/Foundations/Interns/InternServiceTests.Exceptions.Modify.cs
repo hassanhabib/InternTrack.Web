72117ec048dc023b159862b77274b838eee19edf
adi latic <alatic1234@gmail.com>
2023-09-20 15:42:43 -0500

    ShouldThrowCriticalDependencyExceptionOnModifyIfCriticalErrorOccursAndLogItAsync -> PASS

---
+++ InternTrack.Portal.Web.Tests.Unit/Services/Foundations/Interns/InternServiceTests.Exceptions.Modify.cs
@@ -0,0 +1,63 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InternTrack.Portal.Web.Models.Interns;
+using InternTrack.Portal.Web.Models.Interns.Exceptions;
+using Moq;
+using Xunit;
+
+namespace InternTrack.Portal.Web.Tests.Unit.Services.Foundations.Interns
+{
+    public partial class InternServiceTests
+    {
+        [Theory]
+        [MemberData(nameof(CriticalDependencyException))]
+        public async Task ShouldThrowCriticalDependencyExceptionOnModifyIfCriticalErrorOccursAndLogItAsync(
+            Exception criticalDependencyException)
+        {
+            // given
+            Intern someIntern = CreateRandomIntern();
+
+            var failedInternDependencyException =
+                new FailedInternDependencyException(
+                    message: "Failed Intern dependency error occurred, contact support.",
+                        innerException: criticalDependencyException);
+
+            var expectedInternDependencyException =
+                new InternDependencyException(
+                    message: "Intern dependency error occurred, contact support.",
+                        innerException: failedInternDependencyException);
+            
+            this.apiBrokerMock.Setup(broker =>
+                broker.PutInternAsync(It.IsAny<Intern>()))
+                    .ThrowsAsync(criticalDependencyException);
+            
+            // when
+            ValueTask<Intern> modifyInternTask =
+                this.internService.ModifyInternAsync(someIntern);
+
+            // then
+            await Assert.ThrowsAsync<InternDependencyException>(() =>
+               modifyInternTask.AsTask());
+
+            this.apiBrokerMock.Verify(broker =>
+                broker.PutInternAsync(It.IsAny<Intern>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedInternDependencyException))),
+                        Times.Once);
+
+            this.apiBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
