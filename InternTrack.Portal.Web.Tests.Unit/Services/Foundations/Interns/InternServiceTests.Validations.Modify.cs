5db6b18ba56b6324148cf90532452a19c2bfc20a
adi latic <alatic1234@gmail.com>
2023-09-25 17:18:46 -0500

    ShouldThrowValidationExceptionOnModifyIfInternIsInvalidAndLogItAsync -> FAIL

--- InternTrack.Portal.Web.Tests.Unit/Services/Foundations/Interns/InternServiceTests.Validations.Modify.cs
+++ InternTrack.Portal.Web.Tests.Unit/Services/Foundations/Interns/InternServiceTests.Validations.Modify.cs
@@ -19,7 +19,7 @@ namespace InternTrack.Portal.Web.Tests.Unit.Services.Foundations.Interns
     public partial class InternServiceTests
     {
         [Fact]
-        private async void ShouldThrowValidationExceptionOnModifyIfInternIsInvalidAndLogItAsync()
+        private async void ShouldThrowValidationExceptionOnModifyIfInternIsNullAndLogItAsync()
         {
             // given
             Intern nullIntern = null;
@@ -53,6 +53,100 @@ namespace InternTrack.Portal.Web.Tests.Unit.Services.Foundations.Interns
 
             this.apiBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        private async Task ShouldThrowValidationExceptionOnModifyIfInternIsInvalidAndLogItAsync(
+            string invalidText)
+        {
+            // given
+            var innerException = new Exception();
+
+            var invalidIntern = new Intern
+            {
+                FirstName = invalidText
+            };
+
+            var invalidInternException = new InvalidInternException(
+                message: "Invalid Intern. Please correct the errors and try again",
+                    innerException: innerException);
+
+            invalidInternException.AddData(
+                key: nameof(Intern.Id),
+                values: "Id is required");
+
+            invalidInternException.AddData(
+                key: nameof(Intern.FirstName),
+                values: "Text is required");
+
+            invalidInternException.AddData(
+                key: nameof(Intern.LastName),
+                values: "Text is required");
+
+            invalidInternException.AddData(
+                key: nameof(Intern.Email),
+                values: "Text is required");
+
+            invalidInternException.AddData(
+                key: nameof(Intern.PhoneNumber),
+                values: "Text is required");
+
+            invalidInternException.AddData(
+                key: nameof(Intern.Status),
+                values: "Text is required");
+
+            invalidInternException.AddData(
+                key: nameof(Intern.UpdatedDate),
+                values: new String[] { "Date is required",
+                    $"Date is the same as {nameof(Intern.CreatedDate)}"});
+
+            invalidInternException.AddData(
+                key: nameof(Intern.CreatedDate),
+                values: "Date is required");
+
+            invalidInternException.AddData(
+                key: nameof(Intern.JoinDate),
+                values: "Date is required");
+
+            invalidInternException.AddData(
+                key: nameof(Intern.UpdatedBy),
+                values: "Id is required");
+
+            invalidInternException.AddData(
+                key: nameof(Intern.CreatedBy),
+                values: "Id is required");
+
+            var expectedInternValidationException =
+                new InternValidationException(
+                    message: "Intern validation error occurred. Please, try again.",
+                        innerException: invalidInternException);
+
+            // when
+            ValueTask<Intern> modifyInternTask =
+                this.internService.ModifyInternAsync(invalidIntern);
+
+            InternValidationException actualInternValidationException =
+                await Assert.ThrowsAsync<InternValidationException>(
+                    modifyInternTask.AsTask);
+
+            // then
+            actualInternValidationException.Should().BeEquivalentTo(
+                expectedInternValidationException);
+
+            this.apiBrokerMock.Verify(broker =>
+                broker.PutInternAsync(It.IsAny<Intern>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedInternValidationException))),
+                        Times.Once);
+
+            this.apiBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();   
         }
     }
 }
